a5c2a13157fe242c728f699f06f5beea49f3c679
Marcin Wajdzik <wajdzik.martin@gmail.com>
2024-08-03 18:00:12 +0200

    feat: added gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -45,6 +46,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-PCQJGH29" />
       <body className={inter.className}>{children}</body>
     </html>
   );
