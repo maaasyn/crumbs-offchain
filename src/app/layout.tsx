04ac9f0ff820c36b8fc3743c0fe3afbe109f9259
Marcin Wajdzik <wajdzik.martin@gmail.com>
2024-08-03 18:24:04 +0200

    feat: added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -46,6 +47,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-SWZNPK2Q8C" />
       <GoogleTagManager gtmId="GTM-PCQJGH29" />
       <body className={inter.className}>{children}</body>
     </html>
